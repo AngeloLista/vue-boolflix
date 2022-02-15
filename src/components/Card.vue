ea0218fc9ce1add29760c1e64ccbb8baf879ce56
AngeloLista <abstractplayer@gmail.com>
2022-02-15 21:27:11 +0100

    added card dynamic poster image

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -2,7 +2,11 @@
   <ul>
     <li>{{ item.title || item.name }}</li>
     <li>{{ item.original_title || item.original_name }}</li>
-    <li>{{ item.original_language }}</li>
+    <li><img :src="posterUrl" :alt="item.name" /></li>
+    <li>
+      <img v-if="hasFlag" :src="flagSrc" :alt="item.original_language" />
+      <span v-else>{{ item.original_language }}</span>
+    </li>
     <li>{{ item.vote_average }}</li>
   </ul>
 </template>
@@ -11,6 +15,23 @@
 export default {
   name: "Card",
   props: ["item"],
+  data() {
+    return {
+      flags: ["it", "en"],
+      basePosterUrl: "https://image.tmdb.org/t/p/",
+    };
+  },
+  computed: {
+    posterUrl() {
+      return `${this.basePosterUrl}w342${this.item.poster_path}`;
+    },
+    flagSrc() {
+      return require(`@/assets/img/${this.item.original_language}.png`);
+    },
+    hasFlag() {
+      return this.flags.includes(this.item.original_language);
+    },
+  },
 };
 </script>
 
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,10 +1,10 @@
 <template>
   <main>
-    <section id="movies">
+    <section v-if="movies.length > 0" id="movies">
       <h2>Movies</h2>
       <Card v-for="movie in this.movies" :key="movie.id" :item="movie" />
     </section>
-    <section id="series">
+    <section v-if="series.length > 0" id="series">
       <h2>Series</h2>
       <Card v-for="serie in this.series" :key="serie.id" :item="serie" />
     </section>
