4f9cc79727477e2327e2ef48f7ae865dcabe5e14
AngeloLista <abstractplayer@gmail.com>
2022-02-16 16:40:36 +0100

    addeed section placehoder to advice user to search for films or tv shows

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,6 +3,12 @@
     <div class="container my-3">
       <ResultSection id="movies" title="Film" :items="movies" />
       <ResultSection id="series" title="Serie TV" :items="series" />
+      <p
+        class="section-placeholder h1 text-center"
+        v-if="(movies.length && series.length) === 0"
+      >
+        Cerca un Film o una Serie TV!
+      </p>
     </div>
   </main>
 </template>
@@ -18,4 +24,9 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.section-placeholder {
+  font-family: "Martel Sans", sans-serif;
+  font-weight: 900;
+  padding: 50px;
+}
 </style>
